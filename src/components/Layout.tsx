636e602be0ee9089ce99b0f6243eaa3c144e719f
Marco Bobr <marrcokyr@gmail.com>
2023-07-18 20:04:34 +0300

    pre final fixed v4

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 import {refresh} from "@/store/reducers/authSlice";
 
-
+//@ts-ignore
 export default function Layout({children}) {
 
     const dispatch = useDispatch()
