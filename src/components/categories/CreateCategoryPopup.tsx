f712b70833fc34d4278884320b7add6534282cb3
Marco Bobr <marrcokyr@gmail.com>
2023-07-18 19:31:03 +0300

    pre final fixed

--- src/components/categories/CreateCategoryPopup.tsx
+++ src/components/categories/CreateCategoryPopup.tsx
@@ -23,7 +23,7 @@ const CreateCategoryPopup:FC<IPopup> = ({visible= false}) => {
     return (
         <div className={"h-screen w-screen flex items-center justify-center absolute top-0 left-0 z-10 backdrop-blur-lg"} onClick={(e) => {
             if (e.target.closest('div.absolute') == e.target) setCreate(false)
-        }}>>
+        }}>
             <div className={"flex flex-col items-center px-4 py-8 bg-green-600 rounded-3xl"}>
                 <input className={"p-2 color-red-500 bg-gray-800"} type="text" onChange={(e)=>{setName(e.target.value)}} value={name} placeholder={"Назва категорії.."}/>
                 <button onClick={submitCreate} className={"bg-orange-950 rounded-3xl mt-4 p-4"}>Створити</button>
